6a2c660e01c31e2c9fb417e0c8422572ea838780
unknown <goronsinni13@gmail.com>
2024-10-10 04:04:23 -0700

    readme

---
+++ app/views/post/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>daftar post</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore, ipsa consequatur nulla est deserunt quos quam soluta reiciendis. Consequuntur.</p>
+</body>
+</html>
